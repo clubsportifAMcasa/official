ff95f5c6527d99b24bb181b13217487053cdbd39
abdoessordo <abdoessordo01@gmail.com>
2021-11-23 23:53:45 +0100

    added filter by tournament status function

---
+++ scripts/filter_status.js
@@ -0,0 +1,76 @@
+let products_wrapper = document.querySelector('.product-list')
+const product = document.querySelectorAll('.product')
+let display_status = document.querySelector('.display-status')
+
+const ongoing = document.querySelectorAll(".ongoing")
+const finished = document.querySelectorAll(".finished")
+const upcoming = document.querySelectorAll(".upcoming")
+
+const ongoing_btn = document.querySelector("#ongoing_btn")
+const finished_btn = document.querySelector("#finished_btn")
+const upcoming_btn = document.querySelector("#upcoming_btn")
+const all_tournaments = document.querySelector("#all_tournaments_btn")
+
+let status_btns = [ongoing_btn, finished_btn, upcoming_btn, all_tournaments];
+
+
+//filtring function
+const filtred = (list) => {
+    while (products_wrapper.firstChild) {
+        products_wrapper.removeChild(products_wrapper.firstChild);
+    }
+
+    if (list == product) {
+        list.forEach(child => {
+            products_wrapper.appendChild(child)
+        })
+    } else {
+        console.log(list)
+        list.forEach(child => {
+            products_wrapper.appendChild(child)
+        })
+    }
+}
+
+// status btns
+status_btns.forEach((btn, i) => {
+    let product_list;
+    switch (i) {
+        case 0:
+            product_list = ongoing
+            break;
+
+        case 1:
+            product_list = finished
+            break;
+
+        case 2:
+            product_list = upcoming
+            break;
+        
+        case 3:
+            product_list = product
+            break;
+    }
+
+    btn.addEventListener('click', () => {
+        filtred(product_list)
+        switch (i) {
+            case 0:
+                display_status.innerHTML = `Ongoing Tournaments`
+                break;
+    
+            case 1:
+                display_status.innerHTML = `Finished Tournaments`
+                break;
+    
+            case 2:
+                display_status.innerHTML = `Upcoming Tournaments`
+                break;
+            
+            case 3:
+                display_status.innerHTML = `All Tournaments`
+                break;
+        }
+    })
+})
--- scripts/script.js
+++ scripts/script.js
@@ -22,82 +22,39 @@ const three_stars = document.querySelectorAll(".three_stars")
 const four_stars = document.querySelectorAll(".four_stars")
 const five_stars = document.querySelectorAll(".five_stars")
 
-const grid_btn = document.querySelector("#grid-btn")
-const list_btn = document.querySelector("#list-btn")
-
 const one_stars_btn = document.querySelector("#one_stars_btn")
 const two_stars_btn = document.querySelector("#two_stars_btn")
 const three_stars_btn = document.querySelector("#three_stars_btn")
 const four_stars_btn = document.querySelector("#four_stars_btn")
 const five_stars_btn = document.querySelector("#five_stars_btn")
 
-let rating_btns = [one_stars_btn, two_stars_btn, three_stars_btn, four_stars_btn, five_stars_btn, all_stars_btn];
+const grid_btn = document.querySelector("#grid-btn")
+const list_btn = document.querySelector("#list-btn")
+
+
+let rating_btns = [one_stars_btn, two_stars_btn, three_stars_btn, four_stars_btn, five_stars_btn];
+
 
 //filtring function
 const filtred = (list) => {
     while (products_wrapper.firstChild) {
         products_wrapper.removeChild(products_wrapper.firstChild);
     }
+
     if (list == product) {
         list.forEach(child => {
             products_wrapper.appendChild(child)
         })
     } else {
-        list.forEach(categorie => {
-            categorie.forEach(child => {
-                products_wrapper.appendChild(child)
-            })
+        console.log(list)
+        list.forEach(child => {
+            products_wrapper.appendChild(child)
         })
     }
 }
 
-// Rating Stars Buttons
-
-
-rating_btns.forEach((btn, i) => {
-    let product_list;
-    switch (i) {
-        case 0:
-            product_list = [one_stars]
-            break;
-
-        case 1:
-            product_list = [two_stars, three_stars, four_stars, five_stars]
-            break;
-
-        case 2:
-            product_list = [three_stars, four_stars, five_stars]
-            break;
-
-        case 3:
-            product_list = [four_stars, five_stars]
-            break;
-
-        case 4:
-            product_list = [five_stars]
-            break;
-
-        case 5:
-            product_list = product
-            break;
-
-    }
-
-    btn.addEventListener('click', () => {
-        filtred(product_list)
-        if (i === 5) {
-            display_status.innerHTML = `All Products`
-        } else if (i === 0) {
-            display_status.innerHTML = `${i + 1} Stars or less rated products`
-        } else {
-            display_status.innerHTML = `${i + 1} Stars or higher rated products`
-        }
-    })
-})
-
 
 // grid - list Buttons
-
 grid_btn.addEventListener('click', () => {
     products_wrapper.classList.remove('listed')
     seperators.forEach(element => {
@@ -150,16 +107,6 @@ list_btn.addEventListener('click', () => {
         img.classList.remove('product_img')
     })
 })
-
-const test = () =>{
-    console.log("Test");
-}
-
-// product.forEach(p => {
-    
-// })
-
-grid_btn.addEventListener("onmouseover", test)
 
 
 buy_btns.forEach(btn => {
